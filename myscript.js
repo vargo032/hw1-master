1a300ba2d0ff657b2bc78771127919b30f547e12
vargo032 <o-vargas@kellogg.northwestern.edu>
2016-01-24 15:14:56 -0600

    slight changes

---
+++ myscript.js
@@ -0,0 +1,7 @@
+alert("hello, world!");
+var a = 100;
+if ( a > 50) {
+  alert("its more than 50!");
+// code goes here
+  // never use single = sign because that's a var assignment. Instead == is equal
+}
